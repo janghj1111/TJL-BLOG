79eccd9d1596c57a9aaf62ef33e2fc3483d56cc1
janghj1111 <janghj1111@naver.com>
2025-01-17 15:09:47 +0900

    사이드 바 수정

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,26 +4,38 @@ import { viteBundler } from '@vuepress/bundler-vite'
 export default {
     bundler: viteBundler(),
     theme: defaultTheme({
-    nav: [
-        {
-          text: 'About',
-          items: [
-             { text: 'About Me', link: '/about/me' },
-             { text: 'About Blog', link: '/about/blog' } // '/about'으로 설정할경우에 '/about.md'를 렌더링
-          ]
-        }
-      ],
     sidebar: [
-      {
-        title: 'Introduction',
-        path: '/guide/',
-        collapsable: false,
+    //   {
+    //     title: 'About',
+    //     path: '/guide/',
+    //     collapsable: false,
+    //     children: [
+    //       '/guide/getting-started',
+    //       '/guide/customization'
+    //     ]
+    //   }
+    {
+        text: 'About1',
         children: [
-          '/guide/getting-started',
-          '/guide/customization'
+            {
+                text: 'About2',
+                children: [
+                    '/about/blog',
+                    '/about/me'
+                ]
+            }
         ]
       }
-    ]
+    ],
+    // navbar: [
+    //     {
+    //       text: 'About',
+    //       items: [
+    //          { text: 'About Me', link: '/about/me' },
+    //          { text: 'About Blog', link: '/about/blog' } // '/about'으로 설정할경우에 '/about.md'를 렌더링
+    //       ]
+    //     }
+    // ],
   }),
   title: 'Today Jang Learned', // 사이트 제목
   description: '배웠던 것들을 기록하는 개인 블로그입니다.', // 사이트 설명
