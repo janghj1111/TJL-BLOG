3df653a11862fb1fc9921461074b9305efbbac50
janghj1111 <janghj1111@naver.com>
2025-01-17 17:05:15 +0900

    네브바 추가

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -27,6 +27,37 @@ export default {
         ]
       }
     ],
+    navbar: [
+        // nested group - max depth is 2
+        {
+          text: 'Group',
+          children: [
+            {
+              // text: 'SubGroup',
+              children: ['/group/foo.md', '/group/bar.md'],
+            },
+          ],
+        },
+        // control when should the item be active
+        {
+          text: 'Group 2',
+          children: [
+            {
+              text: 'Always active',
+              link: '/',
+              // this item will always be active
+              activeMatch: '/',
+            },
+            {
+              text: 'Active on /foo/',
+              link: '/not-foo/',
+              // this item will be active when current route path starts with /foo/
+              // regular expression is supported
+              activeMatch: '^/foo/',
+            },
+          ],
+        },
+      ],
     // navbar: [
     //     {
     //       text: 'About',
